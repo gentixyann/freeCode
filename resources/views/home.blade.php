10bcda628e13ee260d9646ed76ca3856d352c73b
Gento <a551.return@gmail.com>
2019-09-15 15:44:44 +1200

    google fontをapp.scss、_variables.scssで変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+   <div class="row">
+       <div class="col-3 p-5">
+           <img src="https://vignette.wikia.nocookie.net/dragonball/images/8/82/SS3_FCM.png/revision/latest/scale-to-width-down/323?cb=20160410144508" class="rounded-circle" style="max-height: 300px; width: auto;">
+       </div>
+       <div class="col-9 pt-5">
+           <div><h1>孫悟空</h1></div>
+           <div class="d-flex">
+               <div class="pr-5"><strong>500</strong> post</div>
+               <div class="pr-5"><strong>5k</strong> followers</div>
+               <div class="pr-5"><strong>50</strong> following</div>
+           </div>
+       </div>
+   </div>
 </div>
 @endsection
