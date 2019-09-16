2321e6a84ac5821d0d0ca1ddc42908212f78257d
Gento <a551.return@gmail.com>
2019-09-16 13:51:46 +1200

    usernameをデータベースにも追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,39 @@
                <div class="pr-5"><strong>5k</strong> followers</div>
                <div class="pr-5"><strong>50</strong> following</div>
            </div>
+           <div class="pt-4 font-weight-bold">おっす！！！</div>
+           <div>おらは一体どこまで進化させられるんだーー？？</div>
+           <div><a href="#">www.gokugoku.net</a></div>
+       </div>
+   </div>
+
+   <div class="row pt-5">
+       <div class="col-4 p-3">
+           <img src="https://www.bunkaku.net/wp-content/uploads/2019/03/blog0257-580x400.jpg" class="w-100">
+       </div>
+        <div class="col-4 p-3">
+           <img src="https://www.bunkaku.net/wp-content/uploads/2019/03/blog0256-600x317.jpg" class="w-100">
+       </div>
+        <div class="col-4 p-3">
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy6pe6GvItDOXvaPUtLYKeh4IcI2AUlqgQOCbQWwr5o86K4Yw4" class="w-100">
+       </div>
+        <div class="col-4 p-3">
+           <img src="https://ryskkbysh.com/wp-content/uploads/2018/07/SnapShot5.jpg" class="w-100">
+       </div>
+        <div class="col-4 p-3">
+           <img src="https://ryskkbysh.com/wp-content/uploads/2018/07/SnapShot-2.jpg" class="w-100">
+       </div>
+        <div class="col-4 p-3">
+           <img src="https://www.bunkaku.net/wp-content/uploads/2019/05/blog0406-600x275.jpg" class="w-100">
+       </div>
+       <div class="col-4 p-3">
+           <img src="https://ryskkbysh.com/wp-content/uploads/2018/07/SnapShot3.jpg" class="w-100">
+       </div>
+       <div class="col-4 p-3">
+           <img src="https://ryskkbysh.com/wp-content/uploads/2018/07/SnapShot4.jpg" class="w-100">
+       </div>
+       <div class="col-4 p-3">
+           <img src="https://hibinokoto.net/wp-content/uploads/2018/03/2018y03m28d_212553305.png" class="w-100">
        </div>
    </div>
 </div>
