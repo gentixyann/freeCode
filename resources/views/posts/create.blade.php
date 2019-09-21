4626823ddd1be8c0e220468bc6f548521bd068f7
Gento <a551.return@gmail.com>
2019-09-21 19:47:23 +1200

    createページのhtmlを作成

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,53 @@
 
 @section('content')
 <div class="container">
+  <form>
+    <div class="row">
 
-  
+    <div class="col-8 offset-2">
+
+       <div class="form-group row">
+        <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+           <div class="col-md-6">
+
+                 <input id="caption" 
+                 type="text" 
+                 class="form-control 
+                 @error('caption') is-invalid @enderror" 
+                 caption="caption" value="{{ old('caption') }}" 
+                 autocomplete="caption" autofocus>
+
+                 @error('caption')
+              <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+        @enderror
+
+        </div>
+
+
+        <div class="row">
+          <label for="image" class="col-md-4 col-form-label">Post Image</label>
+          <input id="image" 
+                   type="file" 
+                   class="form-control-file
+                   @error('image') is-invalid @enderror" 
+                   caption="image" value="{{ old('image') }}" 
+                   autocomplete="image" autofocus>
+
+                   @error('image')
+                <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+
+
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  </form>
 </div>
 @endsection
